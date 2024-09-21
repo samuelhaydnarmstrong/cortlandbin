89e0c2a196d5b222fff9392d876b08f1153832de
Samuel Armstrong <samuelhaydnarmstrong@outlook.com>
2024-09-21 20:38:58 +0100

    Moved to use Vercel KV

--- app/server/route.ts
+++ app/server/route.ts
@@ -1,18 +1,19 @@
-let status = "CLEAR";
-let lastUpdated = new Date().toLocaleString();
+import { kv } from "@vercel/kv";
 
 export async function GET() {
+  const status = await kv.get("status");
+  const lastUpdated = await kv.get("lastUpdated");
   return Response.json({ status, lastUpdated });
 }
 
 export async function POST(request: Request) {
   const body = await request.text();
   if (body === "BLOCKED") {
-    status = "BLOCKED";
-    lastUpdated = new Date().toLocaleString();
+    kv.set("status", "BLOCKED");
+    kv.set("lastUpdated", new Date().toLocaleString());
   } else if (body === "CLEAR") {
-    status = "CLEAR";
-    lastUpdated = new Date().toLocaleString();
+    kv.set("status", "CLEAR");
+    kv.set("lastUpdated", new Date().toLocaleString());
   }
   return Response.json("OK");
 }
